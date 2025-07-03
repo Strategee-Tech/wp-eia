7898c8b74d8ecc07db73fd31b7ff419a6ed33273
Strategee <notificaciones@strategee.us>
2025-07-03 10:55:54 -0500

    first sync

--- eia.php
+++ eia.php
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 function eia_admin_notice() {
     ?>
     <div class="notice notice-success is-dismissible">
-        <p>Bienvenido al EIA Plugin</p>
+        <p>Bienvenido al EIA Plugin sync</p>
     </div>
     <?php
 }

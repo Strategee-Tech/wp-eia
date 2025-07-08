61a769693b019e7db418fa6df3df4a4832ef1426
Yeison Mejía Suárez <yeisonmejia@strategee.us>
2025-07-08 17:18:42 -0500

    feature/documents

--- includes/api/googleSheet.php
+++ includes/api/googleSheet.php
@@ -17,16 +17,39 @@ function save_google_sheet($request) {
     $client->setAuthConfig(dirname(__FILE__) . '/../utils/google_sheet_2.0.0/client_secret_1065046989376-tbket75qsb90vg21lejeercjhot7i90t.apps.googleusercontent.com.json');
     $client->setScopes([Google_Service_Sheets::SPREADSHEETS]);
     $client->setAccessType('offline'); // Acceso offline para obtener tokens de actualización
-    $client->setDeveloperKey("AIzaSyD8B9Ff8DG-sNI_iYZvN-i2IHuzcUipUik"); //cuenta notificaciones@strategee.us, password mercadeo93
+    $client->setDeveloperKey("AIzaSyD8B9Ff8DG-sNI_iYZvN-i2IHuzcUipUik");
 
-    
+
+    $service = new Google_Service_Sheets($client);
+
 
     echo "<pre>";
-    print_r($client);
+    print_r($service);
     die(); 
 
+$spreadsheetId = 'TU_ID_AQUI';
+$range = 'Hoja1';
 
-    
+$values = [
+    ['Nombre', 'Correo'],
+    ['Pedro', 'pedro@example.com'],
+];
+
+$body = new Google_Service_Sheets_ValueRange([
+    'values' => $values
+]);
+
+$params = ['valueInputOption' => 'RAW'];
+
+$response = $service->spreadsheets_values->append(
+    $spreadsheetId,
+    $range,
+    $body,
+    $params
+);
+
+echo 'Datos insertados en: ' . $response->getUpdates()->getUpdatedRange();
+
 
 }
 

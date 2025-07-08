d5ea2128ad4f798ef84ef3933e0ba2c1d08c9074
Strategee <notificaciones@strategee.us>
2025-07-08 15:51:29 -0500

    feature/images

--- includes/utils/geminiPost.js
+++ includes/utils/geminiPost.js
@@ -1,4 +1,4 @@
-function geminiPost(imageUrl) {
+async function geminiPost(imageUrl) {
 
     const prompt = `Analiza la siguiente imagen y devuelve un objeto JSON con las siguientes propiedades:
     - title: Un título conciso y descriptivo para la imagen.
@@ -17,9 +17,9 @@ function geminiPost(imageUrl) {
                         text: prompt
                     },
                     {
-                        inline_data: {
+                        file_data: {
                             mime_type: 'image/png',
-                            data: imageUrl
+                            file_uri: imageUrl
                         }
                     }
                 ]
@@ -27,13 +27,13 @@ function geminiPost(imageUrl) {
         ]
     };
 
-    const response = fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyB9Q9OQoIMSoJVz_00P5jsSt3eQmJbSK5c', {
+    const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyB9Q9OQoIMSoJVz_00P5jsSt3eQmJbSK5c', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(requestBody)
     });
-    const data = response.json();
+    const data = await response.json();
     console.log(JSON.stringify(data));
 }

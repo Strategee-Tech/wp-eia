14a1edf9be67d3df438d31af1a6092fffeee3590
Strategee <notificaciones@strategee.us>
2025-07-09 11:11:11 -0500

    feature/images

--- includes/views/gallery.php
+++ includes/views/gallery.php
@@ -12,12 +12,8 @@ require_once WP_EIA_PLUGIN_DIR . 'includes/utils/getPaginatedImages.php';
 
 <?php
 
-
-$selected_folder            = isset( $_GET['folder'] ) ? sanitize_text_field( wp_unslash( $_GET['folder'] ) ) : sanitize_text_field( wp_unslash( '2025/06' ) );
-$selected_folder            = trim( $selected_folder, '/' );
-
 $page                       = isset( $_GET['page'] ) ? sanitize_text_field( wp_unslash( $_GET['page'] ) ) : 1;
-$per_page                   = isset( $_GET['per_page'] ) ? sanitize_text_field( wp_unslash( $_GET['per_page'] ) ) : 10;
+$per_page                   = isset( $_GET['per_page'] ) ? sanitize_text_field( wp_unslash( $_GET['per_page'] ) ) : 20;
 
 $image_data = getPaginatedImages($page, $per_page);
 
@@ -82,23 +78,23 @@ $image_data = getPaginatedImages($page, $per_page);
     </table>
 
 
-    <?php if ( $total_pages > 1 ) : ?>
+    <?php if ( $image_data['total_pages'] > 1 ) : ?>
         <div class="tablenav bottom">
             <div class="tablenav-pages">
-                <span class="displaying-num"><?php printf( esc_html__( '%d elementos', 'tu-textdomain' ), $total_records ); ?></span>
+                <span class="displaying-num"><?php printf( esc_html__( '%d elementos', 'tu-textdomain' ), $image_data['total_records'] ); ?></span>
                 <span class="pagination-links">
                     <?php
                     // Enlace a la primera página
                     $first_page_url = add_query_arg( 'paged', 1, remove_query_arg( 's' ) ); // remove_query_arg('s') para limpiar si hay búsqueda
-                    if ( $current_page_num > 1 ) {
+                    if ( $image_data['current_page'] > 1 ) {
                         echo '<a class="first-page button" href="' . esc_url( $first_page_url ) . '">&laquo;</a>';
                     } else {
                         echo '<span class="tablenav-pages-navspan button disabled" aria-hidden="true">&laquo;</span>';
                     }
 
                     // Enlace a la página anterior
-                    $prev_page_url = add_query_arg( 'paged', $prev_page, remove_query_arg( 's' ) );
-                    if ( $prev_page !== null ) {
+                    $prev_page_url = add_query_arg( 'paged', $image_data['prev_page'], remove_query_arg( 's' ) );
+                    if ( $image_data['prev_page'] !== null ) {
                         echo '<a class="prev-page button" href="' . esc_url( $prev_page_url ) . '">&lsaquo;</a>';
                     } else {
                         echo '<span class="tablenav-pages-navspan button disabled" aria-hidden="true">&lsaquo;</span>';
@@ -106,21 +102,21 @@ $image_data = getPaginatedImages($page, $per_page);
                     ?>
                     <span class="paging-input">
                         <label for="current-page-selector" class="screen-reader-text"><?php esc_html_e( 'Página actual', 'tu-textdomain' ); ?></label>
-                        <input class="current-page" id="current-page-selector" type="text" name="paged" value="<?php echo esc_attr( $current_page_num ); ?>" size="1" aria-describedby="table-paging">
-                        <span class="tablenav-paging-text"> de <span class="total-pages"><?php echo esc_html( $total_pages ); ?></span></span>
+                        <input class="current-page" id="current-page-selector" type="text" name="paged" value="<?php echo esc_attr( $image_data['current_page'] ); ?>" size="1" aria-describedby="table-paging">
+                        <span class="tablenav-paging-text"> de <span class="total-pages"><?php echo esc_html( $image_data['total_pages'] ); ?></span></span>
                     </span>
                     <?php
                     // Enlace a la página siguiente
-                    $next_page_url = add_query_arg( 'paged', $next_page, remove_query_arg( 's' ) );
-                    if ( $next_page !== null ) {
+                    $next_page_url = add_query_arg( 'paged', $image_data['next_page'], remove_query_arg( 's' ) );
+                    if ( $image_data['next_page'] !== null ) {
                         echo '<a class="next-page button" href="' . esc_url( $next_page_url ) . '">&rsaquo;</a>';
                     } else {
                         echo '<span class="tablenav-pages-navspan button disabled" aria-hidden="true">&rsaquo;</span>';
                     }
 
                     // Enlace a la última página
-                    $last_page_url = add_query_arg( 'paged', $total_pages, remove_query_arg( 's' ) );
-                    if ( $current_page_num < $total_pages ) {
+                    $last_page_url = add_query_arg( 'paged', $image_data['total_pages'], remove_query_arg( 's' ) );
+                    if ( $image_data['current_page'] < $image_data['total_pages'] ) {
                         echo '<a class="last-page button" href="' . esc_url( $last_page_url ) . '">&raquo;</a>';
                     } else {
                         echo '<span class="tablenav-pages-navspan button disabled" aria-hidden="true">&raquo;</span>';

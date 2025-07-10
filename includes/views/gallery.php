697691dca82da888da21f851a508e7e9de576dca
Strategee <notificaciones@strategee.us>
2025-07-10 15:49:52 -0500

    feature/images

--- includes/views/gallery.php
+++ includes/views/gallery.php
@@ -121,7 +121,7 @@ function getIconAlt($alt){
     $iconSize = '';
     $colorSize = '';
     switch ($alt) {
-        case $alt != null:
+        case !empty($alt):
         $iconSize = 'dashicons dashicons-yes';
         $colorSize = 'color: #2ECC71;';
         break;

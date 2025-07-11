1bd5d5e4865102c260bd5286901bc80c59fa2b30
Strategee <notificaciones@strategee.us>
2025-07-11 17:12:12 -0500

    feature/images

--- includes/views/gallery.php
+++ includes/views/gallery.php
@@ -331,9 +331,10 @@ function getIconExtension($url){
                             class="edit-attachment-trigger dashicons dashicons-edit"
                             data-attachment-id="<?php echo esc_attr( $image['attachment_id'] ); ?>"
                             data-attachment-title="<?php echo esc_attr( $image['post_title'] ); ?>" data-attachment-alt="<?php echo esc_attr( $image['image_alt_text'] ); ?>" data-attachment-description="<?php echo esc_attr( $image['post_content'] ); ?>" data-attachment-slug="<?php echo esc_attr( basename($image['file_path_relative']) ); ?>" data-attachment-size="<?php echo esc_attr( $image['image_width'] . 'x' . $image['image_height'] ); ?>" data-attachment-url="<?php echo esc_attr( $image['attachment_url'] ); ?>" ></span>
-                        <?php if ( !empty( $image['usage'] ) ) : // Cambié $image['usage'] a !empty($image['usage']) para verificar si está siendo usado ?>
+                        <?php if ( !empty( $image['usage'] ) ) : ?>
                             <span class="dashicons dashicons-trash"></span>
                         <?php endif; ?>
+
                         <a href="<?php echo esc_url( $image['attachment_url'] ); ?>" target="_blank"> <span class="dashicons dashicons-visibility"></span>
                         </a>
                     </td>
@@ -344,6 +345,8 @@ function getIconExtension($url){
         ?>
     </tbody>
 </table>
+
+
 
 
     <?php if ( $image_data['total_pages'] > 1 ) : ?>

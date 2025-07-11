c1433346dc2e2c75ab31bf022a430d317f2b693a
Yeison Mejía Suárez <yeisonmejia@strategee.us>
2025-07-11 07:57:12 -0500

    feature/documents

--- includes/utils/optimizar_imagenes.php
+++ includes/utils/optimizar_imagenes.php
@@ -1,10 +1,23 @@
 <?php
 
 
-function comprimir_imagenes(){
+function comprimir_imagenes($original_path){
+	$temp_img = $original_path;
 
+    // Obtener dimensiones de la imagen
+    [$width, $height] = getimagesize($original_path);
 
-	echo "fsdfsd";
-	die();
+    // Verificar si necesita redimensionarse
+    if ($width > 1920) {
+        $command = "convert " . escapeshellarg($original_path) . " -resize 1920x -quality 80 " . escapeshellarg($temp_img);
+    } else {
+        $command = "convert " . escapeshellarg($original_path) . " -quality 80 " . escapeshellarg($temp_img);
+    }
 
+ 	exec($command, $output, $code);
+ 	if ($code !== 0) {
+        return false
+    } else {
+        return true;
+    }
 }

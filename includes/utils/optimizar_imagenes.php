1ef342e1ea6d7132ad7f0f44a7fe1eb31f30dbc9
Yeison Mejía Suárez <yeisonmejia@strategee.us>
2025-07-11 08:16:32 -0500

    feature/documents

--- includes/utils/optimizar_imagenes.php
+++ includes/utils/optimizar_imagenes.php
@@ -1,23 +1,35 @@
 <?php
 
 
-function comprimir_imagenes($original_path){
-	$temp_img = $original_path;
+function comprimir_imagenes($original_path, $params = []){
+	$info = pathinfo($original_path);
+    $ext = '.webp';
 
-    // Obtener dimensiones de la imagen
+    // Generar nombre del archivo optimizado
+    $temp_img = $info['dirname'] . '/' . $info['filename'] . '-opt' . $ext;
+
+    // Obtener ancho de la imagen original
     [$width, $height] = getimagesize($original_path);
 
-    // Verificar si necesita redimensionarse
-    if ($width > 1920) {
+    // Decidir si redimensionar
+    $shouldResize = isset($params['resize']) && $params['resize'] === true && $width > 1920;
+
+    // Construir el comando
+    if ($shouldResize) {
         $command = "convert " . escapeshellarg($original_path) . " -resize 1920x -quality 80 " . escapeshellarg($temp_img);
+        error_log("Redimensionando y convirtiendo a WebP: $temp_img");
     } else {
         $command = "convert " . escapeshellarg($original_path) . " -quality 80 " . escapeshellarg($temp_img);
+        error_log("Solo conversión a WebP: $temp_img");
     }
 
- 	exec($command, $output, $code);
- 	if ($code !== 0) {
+    // Ejecutar comando
+    exec($command, $output, $code);
+
+    if ($code !== 0) {
+        error_log("Error en la conversión: " . implode("\n", $output));
         return false;
-    } else {
-        return true;
     }
+
+    return true;
 }

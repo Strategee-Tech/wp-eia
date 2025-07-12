0dad54413768b7b56862fe60aca1d937bd803a0b
yeisonmejia1996 <yeisonmejia@strategee.us>
2025-07-12 09:51:28 -0500

    feature/documents

--- includes/utils/optimizar_archivos.php
+++ includes/utils/optimizar_archivos.php
@@ -4,23 +4,38 @@ require_once dirname(__DIR__) . '/utils/auth.php';
 
 function optimizar_archivos($original_path, $params = []) {
     $info       = pathinfo($original_path);
-    $ext        = '.webp';
     $webp_path  = $info['dirname'] . '/' . $info['filename'] . '-opt' . $ext;
 
-    // Obtener tipo MIME
-    $mime = mime_content_type($original_path);
+    if($params['type'] == 'imagen') {
         // Obtener dimensiones
-    [$width, $height] = getimagesize($original_path);
-    $resize = false;
-    if($width > 1920) {
-        $resize = true;
+        [$width, $height] = getimagesize($original_path);
+        $resize = false;
+        $ext    = '.webp';   
+        if($width > 1920) {
+            $resize = true;
+        } 
+        try {
+            call_compress_api($params['type'], $original_path, $webp_path, $resize);
+        } catch (Exception $e) {
+            error_log("Error al comprimir con API externa: " . $e->getMessage());
+            return false;
+        } 
+    } else if($params['type'] == 'multimedia') {
+        try {
+            call_compress_api($params['type'], $original_path, $webp_path);
+        } catch (Exception $e) {
+            error_log("Error al comprimir con API externa: " . $e->getMessage());
+            return false;
+        }
+
+    } else if($params['type'] == 'pdf') {
+        try {
+            call_compress_api($params['type'], $original_path, $webp_path);
+        } catch (Exception $e) {
+            error_log("Error al comprimir con API externa: " . $e->getMessage());
+            return false;
+        }
     } 
-    try {
-        call_compress_api('imagen', $original_path, $webp_path, $resize);
-    } catch (Exception $e) {
-        error_log("Error al comprimir con API externa: " . $e->getMessage());
-        return false;
-    }
 
     if (!file_exists($webp_path)) {
         error_log("El archivo optimizado no fue generado: $webp_path");

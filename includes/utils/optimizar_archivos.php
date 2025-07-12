a41754654df49467d083edd6762a598130961267
yeisonmejia1996 <yeisonmejia@strategee.us>
2025-07-11 20:32:27 -0500

    feature/documents

--- includes/utils/optimizar_archivos.php
+++ includes/utils/optimizar_archivos.php
@@ -3,28 +3,27 @@
 require_once dirname(__DIR__) . '/utils/auth.php';
 
 function optimizar_archivos($original_path, $params = []) {
-    $info      = pathinfo($original_path);
-    $ext       = '.webp';
-    $webp_path = $info['dirname'] . '/' . $info['filename'] . '-opt' . $ext;
+    $info       = pathinfo($original_path);
+    $ext        = '.webp';
+    $webp_path  = $info['dirname'] . '/' . $info['filename'] . '-opt' . $ext;
 
-    // Obtener dimensiones
+    // Obtener tipo MIME
+    $mime = mime_content_type($original_path);
+        // Obtener dimensiones
     [$width, $height] = getimagesize($original_path);
-
-    $resize = isset($params['resize']) && $params['resize'] === true && $width > 1920;
-
-    // Comando ImageMagick
-    if ($resize) {
-        $command = "convert " . escapeshellarg($original_path) . " -resize 1920x -quality 80 " . escapeshellarg($webp_path);
-        error_log("Redimensionando y convirtiendo: $webp_path");
-    } else {
-        $command = "convert " . escapeshellarg($original_path) . " -quality 80 " . escapeshellarg($webp_path);
-        error_log("Solo conversión a WebP: $webp_path");
+    $resize = false;
+    if($width > 1920) {
+        $resize = true;
+    } 
+    try {
+        call_compress_api('imagen', $original_path, $webp_path, $resize);
+    } catch (Exception $e) {
+        error_log("Error al comprimir con API externa: " . $e->getMessage());
+        return false;
     }
 
-    exec($command, $output, $code);
-
-    if ($code !== 0 || !file_exists($webp_path)) {
-        error_log("Error en la conversión a WebP: " . implode("\n", $output));
+    if (!file_exists($webp_path)) {
+        error_log("El archivo optimizado no fue generado: $webp_path");
         return false;
     }
 

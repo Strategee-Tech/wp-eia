7badb9e9cfdaf24a306d5659d35cfc80e392fdf6
yeisonmejia1996 <yeisonmejia@strategee.us>
2025-07-12 11:48:29 -0500

    feature/documents

--- includes/utils/optimizar_archivos.php
+++ includes/utils/optimizar_archivos.php
@@ -5,45 +5,24 @@ require_once dirname(__DIR__) . '/utils/auth.php';
 function optimizar_archivos($original_path, $params = []) {
     $info       = pathinfo($original_path);
     $ext        = $info['extension'];
-
+    $resize     = false;
+    $webp_path  = $info['dirname'] . '/' . $info['filename'] . '-opt.' . $ext;
     if($params['type'] == 'imagen') {
-        // Obtener dimensiones
-        $resize     = false;  
+        [$width, $height] = getimagesize($original_path); 
         $webp_path  = $info['dirname'] . '/' . $info['filename'] . '-opt.webp';
-        [$width, $height] = getimagesize($original_path);
-
         if($width > 1920) {
             $resize = true;
         } 
-        try {
-            call_compress_api($params['type'], $original_path, $webp_path, $resize);
-        } catch (Exception $e) {
-            error_log("Error al comprimir con API externa: " . $e->getMessage());
-            return false;
-        } 
-    } else if($params['type'] == 'multimedia') {
-        $webp_path  = $info['dirname'] . '/' . $info['filename'] . '-opt.' . $ext;
-        try {
-            call_compress_api($params['type'], $original_path, $webp_path);
-        } catch (Exception $e) {
-            error_log("Error al comprimir con API externa: " . $e->getMessage());
-            return false;
-        }
-
-    } else if($params['type'] == 'pdf') {
-        $webp_path  = $info['dirname'] . '/' . $info['filename'] . '-opt.' . $ext;
-        try {
-            call_compress_api($params['type'], $original_path, $webp_path);
-        } catch (Exception $e) {
-            error_log("Error al comprimir con API externa: " . $e->getMessage());
-            return false;
-        }
+    }
+    try {
+        call_compress_api($params['type'], $original_path, $webp_path, $resize);
+    } catch (Exception $e) {
+        error_log("Error al comprimir con API externa: " . $e->getMessage());
+        return false;
     } 
-
     if (!file_exists($webp_path)) {
         error_log("El archivo optimizado no fue generado: $webp_path");
         return false;
     }
-
     return $webp_path;
 }

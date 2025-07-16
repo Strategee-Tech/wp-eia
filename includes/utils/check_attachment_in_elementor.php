976aadaa76e96bbca63143a15b75f5a4da26f656
Strategee <notificaciones@strategee.us>
2025-07-16 16:15:12 -0500

    feature/images

--- includes/utils/check_attachment_in_elementor.php
+++ includes/utils/check_attachment_in_elementor.php
@@ -32,6 +32,8 @@ function check_attachment_in_elementor($attachment_ids = [], $file_paths = [] ) 
         $elementor_data_regex = implode('|', array_map(function($path) {
             $path_parts = pathinfo($path);
             $newPath = $path_parts['dirname'] . '/' . $path_parts['basename'];
+            print_r($newPath);
+            die();
             return str_replace('/', '\\\\/',$newPath); // \/2025\/03\/descarga\.png
         }, $escaped_files));
 

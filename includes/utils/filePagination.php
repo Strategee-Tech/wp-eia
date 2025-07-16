d8afb6673dbb150c0bf3e5b4c38a092b84f8c033
Strategee <notificaciones@strategee.us>
2025-07-16 15:35:40 -0500

    feature/images

--- includes/utils/filePagination.php
+++ includes/utils/filePagination.php
@@ -197,7 +197,7 @@ function getPaginatedFiles( $page = 1, $per_page = 10, $folder = null, $mime_typ
             //$elementor_attachments = check_attachments_in_elementor( $attachments_in_folder );
             $elementor_attachments = check_attachment_in_elementor($id_list, $path_list);
             //$learnpress_attachments = check_attachment_in_learnpress( $path_list );
-            //$content_attachments = check_attachment_in_content( $path_list);
+            $content_attachments = check_attachments_in_content( $path_list);
 
     
             foreach ($attachments_in_folder as &$attachment) {
@@ -216,9 +216,9 @@ function getPaginatedFiles( $page = 1, $per_page = 10, $folder = null, $mime_typ
                 // if($learnpress_attachments[$attachment['attachment_id'] ] == true){
                 //     $attachment['in_programs'] = true;
                 // }
-                //$if($content_attachments[$attachment['file_path_relative'] ] == true){
-                //    $attachment['in_content'] = true;
-                //}
+                if($content_attachments[$attachment['file_path_relative'] ] == true){
+                   $attachment['in_content'] = true;
+                }
 
                 // Determine and update 'in_use' status
                 $current_in_use_status = ($attachment['in_content'] || $attachment['in_programs'] || $attachment['in_elementor']) ? 'En Uso' : 'Sin Uso';

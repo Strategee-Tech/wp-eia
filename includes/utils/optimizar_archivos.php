4520bebb027078cae3af9550785bc22d63897afa
yeisonmejia1996 <yeisonmejia@strategee.us>
2025-07-18 18:49:13 -0500

    feature/documents

--- includes/utils/optimizar_archivos.php
+++ includes/utils/optimizar_archivos.php
@@ -60,12 +60,101 @@ function reemplazar_archivo_optimizado($upload, $original_path, $optimized_path,
         $upload['url']  = str_replace(basename($original_path), basename($optimized_path), $upload['url']);
         $upload['type'] = $forced_mime ?: mime_content_type($optimized_path);
 
-        echo "<pre>";
-        print_r($upload);
-        die(); 
+        $geminiData = getInfoGemini($upload['url']);
+
+        if (is_array($geminiData) && isset($geminiData[0])) { 
+            if(!empty($geminiData[0])) { 
+
+                $slug = sanitize_file_name($geminiData[0]['slug']) . '.webp';
+
+                //Ruta actual del archivo
+                $current_path = $upload['file']; // Ej: /var/www/.../uploads/2025/07/original.webp
+
+                //Directorio actual (donde está el archivo)
+                $dir = dirname($current_path); // Ej: /var/www/.../uploads/2025/07
+
+                //Nueva ruta física
+                $new_path = $dir . '/' . $slug;
+
+                //Renombrar el archivo en el servidor
+                if (rename($current_path, $new_path)) {
+                    //Construir nueva URL
+                    $upload['url']  = trailingslashit(dirname($upload['url'])) . $slug;
+
+                    //Actualizar file y type
+                    $upload['file'] = $new_path; 
+                } else {
+                    error_log('Error al renombrar el archivo a: ' . $new_path);
+                } 
+
+                $gemini_data = [
+                    'alt_temp'         => $geminiData['0']['alt'],
+                    'slug_temp'        => $geminiData['0']['slug'],
+                    'description_temp' => $geminiData['0']['description'],
+                    'title_temp'       => $geminiData['0']['title'],
+                ]; 
+                $unique_key = 'gemini_' . md5($upload['url']); 
+                set_transient($unique_key, $gemini_data, 5 * MINUTE_IN_SECONDS);
+            }
+        } 
         @unlink($original_path);
     }
     return $upload;
+}
+
+add_action('add_attachment', 'update_attachment_with_gemini_data');
+
+function update_attachment_with_gemini_data($attachment_id) {
+    $post = get_post($attachment_id);
+    if ($post->post_type !== 'attachment') return;
+
+    $mime = get_post_mime_type($attachment_id);
+    if (strpos($mime, 'image/') !== 0) return;
+
+    $current_file_path = get_attached_file($attachment_id);
+    if (!$current_file_path || !file_exists($current_file_path)) {
+        //error_log("Archivo adjunto no encontrado en: " . $current_file_path);
+        return;
+    }
+    $url         = wp_get_attachment_url($attachment_id);
+    $unique_key  = 'gemini_' . md5($url);
+    $gemini_data = get_transient($unique_key); 
+
+    if ($gemini_data) {
+
+        $update_post_args = [
+            'ID'           => $attachment_id,
+            'post_title'   => $gemini_data['title_temp'],
+            'post_content' => $gemini_data['description_temp'],
+        ]; 
+        wp_update_post($update_post_args);
+        update_post_meta($attachment_id, '_wp_attachment_image_alt', $gemini_data['alt_temp']);
+        
+        // Luego bórralo para no dejar basura
+        delete_transient($unique_key);
+    } 
+}
+
+function getInfoGemini($url){
+    require_once(dirname(ABSPATH) . '/credentials.php');
+
+    $endpoint = site_url('/wp-json/api/v1/gemini');
+    $username = AUTH_USER_BASIC;
+    $password = AUTH_PASSWORD_BASIC;
+
+    $data = ['imageUrl' => $url];
+    $ch   = curl_init($endpoint);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
+
+    $response = curl_exec($ch);
+
+    curl_close($ch);
+
+    return json_decode($response, true); 
 }
 
 function optimizar_archivos($original_path, $params = []) {

5dda9a1f62aa7e25c93db40366340399bf43f0b5
yeisonmejia1996 <yeisonmejia@strategee.us>
2025-07-18 22:46:19 -0500

    feature/documents

--- includes/helpers/getEnclosure.php
+++ includes/helpers/getEnclosure.php
@@ -12,6 +12,11 @@ function get_enclosure($path) {
          WHERE pm.meta_key = 'enclosure'
            AND pm.meta_value REGEXP %s
            AND p.post_status IN ('publish','private','draft')
+           AND p.post_type IN (
+            'post','page','custom_post_type','lp_course','service','portfolio',
+            'gva_event','gva_header','footer','team','elementskit_template',
+            'elementskit_content','elementor_library'
+          )
          LIMIT 1",
         $pattern
     );

ddfea997812f1bd82a8700e79ad41f4bcf99cd56
Yeison Mejía Suárez <yeisonmejia@strategee.us>
2025-07-23 07:36:22 -0500

    feature/documents

--- includes/views/gallery.php
+++ includes/views/gallery.php
@@ -389,7 +389,7 @@ function getIconAlt($alt){
                 <label for="modal-fast-edit">Edición rápida</label>
             </div>
             <div style="text-align: right; font-size: 12px; margin-bottom: 10px; margin-top: 2px;">
-                <small id="fast_edit_option">Si se selecciona la opción de edicion rápida, solo se actualizará el texto alternativo (alt), título y descripción de la imagen</small>
+                <small id="fast_edit_option">Si se selecciona la opción de edicion rápida, solo se actualizará el texto alternativo (alt), título y descripción de la imagen.</small>
                 <small style="display: none;" id="complete_edit">Se actualizará el texto alternativo (alt), título, descripción y se optimizará la imagen. Esto puede tardar unos minutos.</small>
             </div>
             
@@ -418,9 +418,6 @@ function getIconAlt($alt){
     </div>
 </div>
 
-
-
- 
 
 <?php
 
